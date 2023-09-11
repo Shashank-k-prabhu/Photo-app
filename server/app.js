a2eff85347e53b2fa02f899255abcfa1ed821cd9
Shashank K Prabhu <shashankkp02@gmail.com>
2023-09-11 21:46:42 +0530

    Migrated front-end compnents to client and created backend component as server

---
+++ server/app.js
@@ -0,0 +1,22 @@
+// import modules
+  const express= import("express");
+  const mogoose=import("mongoose");
+  const morgan=import("morgan");
+  const
+
+//app
+
+
+
+// db
+
+
+// middlerware
+
+
+
+//routes
+
+
+
+//port
