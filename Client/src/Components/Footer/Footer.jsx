6922902fd78e721b5ce6a1a82fd89952d91789a1
sneha <snehavsarah@gmail.com>
2023-09-15 14:29:25 +0530

    Done with the footer section

--- Client/src/Components/Footer/Footer.jsx
+++ Client/src/Components/Footer/Footer.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import "./Footer.css";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faInstagram, faFacebook, faTwitter, faLinkedin , faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
     return (
     <footer className="footer">
-        <div className="footer-content">
-        <p>&copy; 2023  PIXELPALS</p>
+        <div className="container">
+            <div className="social-icons">
+                <a href="#">
+                    <FontAwesomeIcon icon={faInstagram}/>
+                </a>
+                <a  href="#">
+                    <FontAwesomeIcon icon={faFacebook}/>
+                </a>
+                <a href="#">
+                    <FontAwesomeIcon icon={faTwitter} /> {/* New Twitter icon */}
+                </a>
+                <a href="#">
+                    <FontAwesomeIcon icon={faLinkedin} /> {/* New LinkedIn icon */}
+                </a>
+                <a href="#">
+                    <FontAwesomeIcon icon={faGithub}/>                
+                </a>
+            </div>
+            <div>
+                <p>&copy; {new Date().getFullYear()} PIXELPALS</p>
+                <p>Designed by Shashank & Sneha</p>
+            </div>
         </div>
     </footer>
 );
