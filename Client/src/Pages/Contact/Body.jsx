d49853d4825c143be03bffffb6ceb2030ce3775a
sneha <snehavsarah@gmail.com>
2023-09-16 16:48:18 +0530

    Updated portfolio page and contact page

--- Client/src/Pages/Contact/Body.jsx
+++ Client/src/Pages/Contact/Body.jsx
@@ -2,19 +2,18 @@ import React from "react";
 import EnquiryForm from "./EnquiryForm";
 import contactimage from "../../Components/Images/contact_page.png";
 import "./Body.css";
+import "./EnquiryForm.css"
 
 function ContactBody() {
   return (
-    <div className="container">
+    <div className="body-container">
       <div className="background-container">
         <img src={contactimage} alt="Pic" className="imagestyles" />
       </div>
       <div className="form-align">
-        <div className="form-div">
           <form className="form">
             <EnquiryForm />
           </form>
-        </div>
       </div>
     </div>
   );
