f0aed489b243597994c1ca5a619113a2a2da3383
sneha <snehavsarah@gmail.com>
2023-09-18 00:48:40 +0530

    email

--- Client/src/Components/Body/body.jsx
+++ Client/src/Components/Body/body.jsx
@@ -5,7 +5,8 @@ function LandingBody(){
     <div className="home-container">
       <img src={landingImage} alt="Landing Pic" className="imagestyles"></img>
       <p className="bodytext">
-        Welcome to PixelPals!!Your photography destination! Discover a diverse
+        Welcome to PixelPals!!
+        Your photography destination! Discover a diverse
         range of talented photographers, their contact details, and stunning
         galleries. Fulfill your photography needs anytime, anywhere. Explore and
         capture moments with us!"
