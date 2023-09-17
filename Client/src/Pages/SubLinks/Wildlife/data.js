c798afc5fa59a7c1363a4a853adc052feda854a2
sneha <snehavsarah@gmail.com>
2023-09-17 22:56:44 +0530

    Fashion portfolio completed

--- Client/src/Pages/SubLinks/Wildlife/data.js
+++ Client/src/Pages/SubLinks/Wildlife/data.js
@@ -1,61 +1,61 @@
  export const itemData = [
   {
-    img: "https://images.unsplash.com/photo-1549388604-817d15aa0110",
+    img: "http://localhost:3000/Images/fashion1549388604-817d15aa0110",
     title: "Bed",
     description: "swabdesign",
   },
   {
-    img: "https://images.unsplash.com/photo-1525097487452-6278ff080c31",
+    img: "http://localhost:3000/Images/fashion1525097487452-6278ff080c31",
     title: "Books",
     description: "Pavel Nekoranec",
   },
   {
-    img: "https://images.unsplash.com/photo-1523413651479-597eb2da0ad6",
+    img: "http://localhost:3000/Images/fashion1523413651479-597eb2da0ad6",
     title: "Sink",
     description: "Charles Deluvio",
   },
   {
-    img: "https://images.unsplash.com/photo-1563298723-dcfebaa392e3",
+    img: "http://localhost:3000/Images/fashion1563298723-dcfebaa392e3",
     title: "Kitchen",
     description: "Christian Mackie",
   },
   {
-    img: "https://images.unsplash.com/photo-1588436706487-9d55d73a39e3",
+    img: "http://localhost:3000/Images/fashion1588436706487-9d55d73a39e3",
     title: "Blinds",
     description: "Darren Richardson",
   },
   {
-    img: "https://images.unsplash.com/photo-1574180045827-681f8a1a9622",
+    img: "http://localhost:3000/Images/fashion1574180045827-681f8a1a9622",
     title: "Chairs",
     description: "Taylor Simpson",
   },
   {
-    img: "https://images.unsplash.com/photo-1530731141654-5993c3016c77",
+    img: "http://localhost:3000/Images/fashion1530731141654-5993c3016c77",
     title: "Laptop",
     description: "Ben Kolde",
   },
   {
-    img: "https://images.unsplash.com/photo-1481277542470-605612bd2d61",
+    img: "http://localhost:3000/Images/fashion1481277542470-605612bd2d61",
     title: "Doors",
     description: "Philipp Berndt",
   },
   {
-    img: "https://images.unsplash.com/photo-1517487881594-2787fef5ebf7",
+    img: "http://localhost:3000/Images/fashion1517487881594-2787fef5ebf7",
     title: "Coffee",
     description: "Jen P.",
   },
   {
-    img: "https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee",
+    img: "http://localhost:3000/Images/fashion1516455207990-7a41ce80f7ee",
     title: "Storage",
     description: "Douglas Sheppard",
   },
   {
-    img: "https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62",
+    img: "http://localhost:3000/Images/fashion1597262975002-c5c3b14bbd62",
     title: "Candle",
     description: "Fi Bell",
   },
   {
-    img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+    img: "http://localhost:3000/Images/fashion1519710164239-da123dc03ef4",
     title: "Coffee table",
     description: "Hutomo Abrianto",
   },
