610a4c46826efe4a1e9f4ca4a9c7357e2e6f2a71
sneha <snehavsarah@gmail.com>
2023-09-19 22:04:09 +0530

    popup completed

--- Client/src/Components/Email_sub/Email_sub.jsx
+++ Client/src/Components/Email_sub/Email_sub.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import './Email_sub.css';
 import emailimage from "../Images/email.jpeg";
+
 function Popup(props){
-    return(props.trigger) ? (
+    return (props.trigger) ? (
         <div className="popup">
-            <button className="close-btn" onClick={props.onClose}>X</button>
             <div className="popup-inner">
-                <h2>We really want you to want us</h2>
-                <div className="image-container">
-                <img src={emailimage}  alt="" className="popup-image" />
+                <img src={emailimage} alt="" className="popup-image" />
+                <h2>We really want you to want us! Subscribe below</h2>
+                <div className="email-container">
+                    <input type="email" placeholder="Email" className="email-input" />
+                    <button type="submit" className="sub">SUBMIT</button>
                 </div>
-                <p className="small-text">Enter your email address below:</p>
-                <input type="email" placeholder="Email" className="email-input" />
+                
             </div>
+            <button className="close" onClick={props.onClose}>X</button>
         </div>
     ) : null;
 }
